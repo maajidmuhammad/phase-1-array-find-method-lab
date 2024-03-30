87305ff90505407274f12b282707ffe2af5cba34
maajidmuhammad <abdimohamed0742@gmail.com>
2024-03-30 15:06:29 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(superBowlResult){
+    const winingYear = superBowlResult.find(record => record.result === 'W');
+    return winingYear ? winingYear.year : undefined
+}
+
+const record = [
+    {year: '2012', result: 'l'},
+    {year: '2015', result: 'W'},
+    {year: '2013', result: 'NIL'},
+    {year: '2011', result: 'L'},
+]
+
+console.log(superbowlWin(record))
